4c3165d78034d2200c1f480203858b9677ce1fc5
Stanislav Lesnikov <stan@nashdot.com>
2016-01-11 00:02:21 +0100

    10. Production bundle: Move libs to its own bundle

--- gulp/config.js
+++ gulp/config.js
@@ -16,13 +16,16 @@ config.paths.src.base = 'src';
 config.paths.src.main = path.join(config.paths.src.base, 'client', 'main');
 config.paths.src.systemjs = ['jspm_packages/system.js'];
 config.paths.src.html = 'index.html';
+config.paths.src.libs = 'react + react-dom';
+config.paths.src.libsRev = 'react - react-dom';
 
 config.paths.dist.base = 'dist';
 config.paths.dist.js = path.join(config.paths.dist.base, 'js');
 config.paths.dist.app = 'app.js';
+config.paths.dist.lib = 'lib.js';
 
 config.htmlReplace = {
-  js: ['/js/system.js', '/js/config.js', '/js/app.js'],
+  js: ['/js/system.js', '/js/config.js', '/js/lib.js', '/js/app.js'],
   entry_point: `<script>System.import("${config.paths.src.main}");</script>`
 };
 
--- gulp/scripts.js
+++ gulp/scripts.js
@@ -11,7 +11,8 @@ export function dist() {
         mangle: true
       },
       bundles: [
-        { src: config.paths.src.main, dst: config.paths.dist.app }
+        { src: config.paths.src.main + ' - ' + config.paths.src.libsRev, dst: config.paths.dist.app },
+        { src: config.paths.src.libs, dst: config.paths.dist.lib }
       ]
     })
     .pipe(gulp.dest(config.paths.dist.js));
